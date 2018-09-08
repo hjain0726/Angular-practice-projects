cbda546c5fda42daaa067db56f394ada28bf4cc8
hjain0726 <hjain0726@gmail.com>
2018-09-08 15:16:54 +0530

    template driven approach

--- forms-practice/practice/src/app/app.component.ts
+++ forms-practice/practice/src/app/app.component.ts
@@ -7,15 +7,47 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupform:NgForm;
+  @ViewChild('f') signupform: NgForm;
+  defaultQuestion = "pet";
+  answer = '';
+  genders = ['male', 'female'];
+  submitted = false;
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupform.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+    /*this.signupform.setValue({
+      userData:{
+        username:suggestedName,
+        email:''
+      },
+      secret:'pet',
+      questionanswer:'',
+      gender:'male'
+    });*/
   }
   /*onSubmit(form:NgForm){
     console.log(form);
   }*/
-  onSubmit(){
+  onSubmit() {
     console.log(this.signupform);
+    this.submitted = true;
+    this.user.username = this.signupform.value.userData.username;
+    this.user.email = this.signupform.value.userData.email;
+    this.user.secretQuestion = this.signupform.value.secret;
+    this.user.answer = this.signupform.value.questionanswer;
+    this.user.gender = this.signupform.value.gender;
+    this.signupform.reset();
   }
 }
