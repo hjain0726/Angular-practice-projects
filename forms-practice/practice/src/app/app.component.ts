50a17dc9c6a1f59c3db815260a604f1a2bdf2346
hjain0726 <hjain0726@gmail.com>
2018-10-25 16:26:04 +0530

    reactive approach

--- forms-practice/practice/src/app/app.component.ts
+++ forms-practice/practice/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('f') signupform: NgForm;
+export class AppComponent implements OnInit {
+  //Rective approach
+
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });
+  }
+
+
+
+  // Template driven approach
+
+  /*@ViewChild('f') signupform: NgForm;
   defaultQuestion = "pet";
   answer = '';
   genders = ['male', 'female'];
@@ -19,27 +36,6 @@ export class AppComponent {
     answer: '',
     gender: ''
   };
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    this.signupform.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    });
-    /*this.signupform.setValue({
-      userData:{
-        username:suggestedName,
-        email:''
-      },
-      secret:'pet',
-      questionanswer:'',
-      gender:'male'
-    });*/
-  }
-  /*onSubmit(form:NgForm){
-    console.log(form);
-  }*/
   onSubmit() {
     console.log(this.signupform);
     this.submitted = true;
@@ -49,5 +45,26 @@ export class AppComponent {
     this.user.answer = this.signupform.value.questionanswer;
     this.user.gender = this.signupform.value.gender;
     this.signupform.reset();
-  }
+  }*/
+  /*suggestUserName() {
+    const suggestedName = 'Superuser';
+    this.signupform.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+    //this.signupform.setValue({
+    // userData:{
+    //    username:suggestedName,
+    //    email:''
+    //  },
+    //  secret:'pet',
+    //  questionanswer:'',
+    //  gender:'male'
+    });
+}*/
+  /*onSubmit(form:NgForm){
+    console.log(form);
+  }*/
+
 }
